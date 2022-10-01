f65284e42e24cb2de3d82ef760b27e5393f72b0b
Z-mh666 <78141281+Z-mh666@users.noreply.github.com>
2022-10-01 21:25:06 +0800

    Create jacobi.cpp
    methode de jacobi

---
+++ jacobi.cpp
@@ -0,0 +1,124 @@
+//   g++ jacobi.cpp
+// Execution:
+//   ./a.out 'Nx' 'Ny' 'L'
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+double a = 1.;
+double b = 1.;
+
+//fonction V
+double V(double y){
+  return 1-cos(2*M_PI*y/b);
+}
+
+//solution exacte
+double u(double x,double y){
+  return sin(2*M_PI*x)*sin(2*M_PI*y);
+}
+
+//second membre
+double f(double x,double y){
+  return -8*M_PI*M_PI*sin(2*M_PI*x)*sin(2*M_PI*y);
+}
+
+
+int main(int argc, char* argv[]){
+    // Problem parameters
+  if (argc!=4){
+    cout << "You need to input 3 variables: Nx, Ny and L, here there are " << argc-1 << " variables" << endl;
+    return 1;
+  }
+  int Nx = atoi(argv[1]);
+  int Ny = atoi(argv[2]);
+  int L = atoi(argv[3]);
+  
+  double dx = a/(Nx+1.);
+  double dy = b/(Ny+1.);
+  double h1 = 1./dx/dx;
+  double h2 = 1./dy/dy;
+  
+  // Memory allocation + Initial solution
+  double* fx = new double[(Nx+2)*(Ny+2)];  //second membre
+  double* sol = new double[(Nx+2)*(Ny+2)];  //solution approchee
+  double* sol_ex = new double[(Nx+2)*(Ny+2)];  //solution exacte
+  double* x = new double[(Nx+2)*(Ny+2)];  //vecteur initial
+  double coef = 1./(-2.*h1-2.*h2);  //coefficients diagonaux
+  double err;  //erreur
+  double U0 = 0.5;
+  double alpha = 0.5;
+
+  //Boundary conditions
+  for (int i=0;i<Nx+2;i++){
+    for (int j=0;j<Ny+2;j++){
+      if((j==0)||(j==Ny+1)||(i==Nx+1)){
+        x[i+j*(Ny+2)] = U0;
+        sol[i+j*(Ny+2)] = U0;
+        sol_ex[i+j*(Ny+2)] = U0;
+      }
+      else if (i==0){
+        x[i+j*(Ny+2)] = U0*(1+alpha*V(j*dy));
+        sol[i+j*(Ny+2)] = U0*(1+alpha*V(j*dy));
+        sol_ex[i+j*(Ny+2)] = U0*(1+alpha*V(j*dy));
+      }
+      else{
+        x[i+j*(Ny+2)] = 1.;
+        sol_ex[i+j*(Ny+2)] = u(i*dx,j*dy);
+      }
+      fx[i+j*(Ny+2)] = f(i*dx,j*dy);
+    }
+  }
+
+
+  //Time loop
+  for (int n=0;n<L;n++){
+    // Spatial loop
+    for (int i=1;i<Nx+1;i++){
+      for (int j=1;j<Ny+1;j++){
+        sol[j+i*(Nx+2)] = -coef*(h1*(x[j+i*(Nx+2)-1]+x[j+i*(Nx+2)+1])+h2*(
+        x[j+i*(Nx+2)-(Nx+2)]+x[j+i*(Nx+2)+Nx+2])-fx[j+i*(Nx+2)]);
+      }
+    }
+
+    // Switch pointers
+    double* tmp;
+    tmp = x;
+    x = sol;
+    sol = tmp;
+  }
+  cout<<"[";
+
+  for (int i=0;i<(Nx+2)*(Ny+2);i++){
+    cout<<sol[i]<<',';
+  }
+  cout<<']';
+
+  // Print solution
+  ofstream file;
+  file.open("jacobi_rezu.dat");
+  for (int n=0; n<(Nx+2)*(Ny+2); n++){
+    file << sol[n] << endl;
+  }
+  file.close();
+
+  //calcul d'erreur
+  for (int i=0;i<(Nx+2)*(Ny+2);i++){
+    err += (sol_ex[i]-sol[i])*(sol_ex[i]-sol[i]);
+  }
+  err = sqrt(err);
+
+  cout << "Absolute error: " << err << endl;
+
+  // Memory deallocation
+  delete[] fx;
+  delete[] sol;
+  delete[] x;
+  delete[] sol_ex;
+
+  return 0;
+}
+
